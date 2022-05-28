ac1eda2b1b7959072ecc4a8fa1c315642101dc2d
MICKNICK911 <appiahm29@yahoo.com>
2022-05-28 00:20:27 +0000

    webhooking

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import config
 import models
 import functions
 import database
+import os
+from flask import Flask, request
 
 bot = telebot.TeleBot(config.SECRET_KEY, parse_mode=None)
+server = Flask(__name__)
 
 
 def main():
@@ -270,7 +273,21 @@ def main():
                 start.clear()
                 start.append(5)
 
-    bot.infinity_polling(interval=0, timeout=0)
+    @server.route('/' + config.SECRET_KEY, methods=['POST'])
+    def get_message():
+        json_string = request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return "!", 200
+
+    @server.route("/")
+    def webhook():
+        bot.remove_webhook()
+        bot.set_webhook(url='https://telebot-2o-trial.herokuapp.com/' + config.SECRET_KEY)
+        return "!", 200
+
+    # bot.infinity_polling(interval=0, timeout=0)
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 
 
 if __name__ == '__main__':
