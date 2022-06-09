c84843ccc669b9ac99f86a383380a36f20072e81
MICKNICK911 <appiahm29@yahoo.com>
2022-06-09 15:00:06 +0000

    webhook 2

--- main.py
+++ main.py
@@ -322,7 +322,7 @@ bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
 # Start flask server
 app.run(host=WEBHOOK_LISTEN,
         port=WEBHOOK_PORT,
-        ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV),
+        ssl_context=(WEBHOOK_SSL_CERT,),
         debug=True)
 # bot.infinity_polling()
 
